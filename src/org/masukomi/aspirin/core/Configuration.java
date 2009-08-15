3014e5aa17664e27e469c984e0266d854e4de344
Laszlo Solova <laszlo@solova.com>
2009-08-15 12:07:43 +0200

    Refactored Configuration to allow JMX connections and to support new features. Backward compatible, but old methods are deprecated. Please use the new parameters and parameter names instead.

--- src/org/masukomi/aspirin/core/Configuration.java
+++ src/org/masukomi/aspirin/core/Configuration.java
@@ -22,6 +22,9 @@
  * SOFTWARE.
  */
 package org.masukomi.aspirin.core;
+import java.util.Properties;
+
+import javax.mail.Transport;
 import javax.mail.internet.ParseException;
 
 import org.apache.commons.logging.Log;
@@ -30,29 +33,112 @@ import org.apache.mailet.MailAddress;
 
 /**
  * <p>This class represents the configuration of Aspirin. You can configure this 
- * software two ways
+ * software two ways:</p>
  * 
+ * <ol>
+ *   <li>Get the configuration instance and set parameters.</li>
+ *   <li>Get the instance and initialize with a Properties object.</li>
+ * </ol>
+ * 
+ * <p>There is a way to change behavior of Aspirin dinamically. You can use 
+ * JMX to change configuration parameters. In the parameters list we marked the 
+ * parameters which are applied immediately. For more informations view 
+ * {@link ConfigurationMBean}.</p>
+ * 
+ * <table border="1">
+ *   <tr>
+ *     <th>Name</th>
+ *     <th>Deprecated name</th>
+ *     <th>Type</th>
+ *     <th>Description</th>
+ *   </tr>
+ *   <tr>
+ *     <td>aspirin.delivery.attempt.delay</td>
+ *     <td>aspirinRetryInterval</td>
+ *     <td>Integer</td>
+ *     <td>The delay of next attempt to delivery in milliseconds. <i>Change by JMX applied immediately.</i></td>
+ *   </tr>
+ *   <tr>
+ *     <td>aspirin.delivery.attempt.count</td>
+ *     <td>aspirinMaxAttempts</td>
+ *     <td>Integer</td>
+ *     <td>Maximal number of delivery attempts of an email. <i>Change by JMX applied immediately.</i></td>
+ *   </tr>
+ *   <tr>
+ *     <td>aspirin.delivery.debug</td>
+ *     <td></td>
+ *     <td>Boolean</td>
+ *     <td>If true, full SMTP communication will be logged.</td>
+ *   </tr>
+ *   <tr>
+ *     <td>aspirin.delivery.threads.active.max</td>
+ *     <td>aspirinDeliverThreads</td>
+ *     <td>Integer</td>
+ *     <td>Maximum number of active delivery threads in the pool.</td>
+ *   </tr>
+ *   <tr>
+ *     <td>aspirin.delivery.threads.idle.max</td>
+ *     <td>aspirinDeliverThreads</td>
+ *     <td>Integer</td>
+ *     <td>Maximum number of idle delivery threads in the pool (the deilvery 
+ *     threads over this limit will be shutdown).</td>
+ *   </tr>
+ *   <tr>
+ *     <td>aspirin.delivery.timeout</td>
+ *     <td></td>
+ *     <td>Integer</td>
+ *     <td>Socket and {@link Transport} timeout in milliseconds.</td>
+ *   </tr>
+ *   <tr>
+ *     <td>aspirin.encoding</td>
+ *     <td></td>
+ *     <td>String</td>
+ *     <td>The MIME encoding.</td>
+ *   </tr>
+ *   <tr>
+ *     <td>aspirin.hostname</td>
+ *     <td>aspirinHostname</td>
+ *     <td>String</td>
+ *     <td>The hostname.</td>
+ *   </tr>
+ *   <tr>
+ *     <td>aspirin.logger.name</td>
+ *     <td></td>
+ *     <td>String</td>
+ *     <td>The name of the logger. <i>Readonly.</i></td>
+ *   </tr>
+ *   <tr>
+ *     <td>aspirin.logger.prefix</td>
+ *     <td></td>
+ *     <td>String</td>
+ *     <td>The prefix of the logger. This will be put in the logs at the first position.</td>
+ *   </tr>
+ *   <tr>
+ *     <td>aspirin.postmaster.email</td>
+ *     <td>aspirinPostmaster</td>
+ *     <td>String</td>
+ *     <td>The email address of the postmaster.</td>
+ *   </tr>
+ * </table>
  * 
  * @author Kate Rhodes masukomi at masukomi dot org
  * @version $Id$
  */
-public class Configuration {
-//	static protected long retryInterval = 21600000; // 6 hours default
-	// TODO set log name dinamically
-	private static Log log = LogFactory.getLog("MailService");
-	protected MailAddress postmaster;
-	//private XMLPreferences prefs;
-	static private Configuration instance;
-	private long retryInterval = 300000;
-	private int deliveryThreads = 3;
-	private int maxAttempts = 3;
-	private int connectionTimeout = 30000; // in milliseconds
-	private String encoding = "UTF-8";
-	private String loggerName = "Aspirin";
-	private String logPrefix = "Aspirin ";
+public class Configuration implements ConfigurationMBean {
 	
-	private String hostname = "localhost";
-	private boolean debugCommunication = false;
+	private static Configuration instance;
+	private int maxAttempts = 3; // aspirin.delivery.attempt.count
+	private long retryInterval = 300000; // aspirin.delivery.attempt.delay
+	private boolean debugCommunication = false; // aspirin.delivery.debug
+	private String hostname = "localhost"; // aspirin.delivery.hostname
+	private int deliveryThreads = 3; // aspirin.delivery.threads.active.max
+	private int idleDeliveryThreads = deliveryThreads; // aspirin.delivery.threads.idle.max
+	private int connectionTimeout = 30000; // in milliseconds, aspirin.delivery.timeout
+	private String encoding = "UTF-8"; // aspirin.encoding
+	private static String loggerName = "Aspirin"; // aspirin.logger.name
+	private static Log log = LogFactory.getLog(loggerName); // inherited from aspirin.logger.name
+	private String loggerPrefix = "Aspirin "; // aspirin.logger.prefix
+	protected MailAddress postmaster = null; // inherited from aspirin.postmaster.email
 
 	static public Configuration getInstance() {
 		if (instance == null) {
@@ -61,47 +147,150 @@ public class Configuration {
 		return instance;
 	}
 	
+	public void init(Properties props) {
+		String tempString = null;
+		
+		tempString = props.getProperty(PARAM_DELIVERY_ATTEMPT_DELAY);
+		if( tempString != null )
+		{
+			retryInterval = Long.valueOf(tempString);
+		}else
+		{
+			// We need this to support backward compatibility
+			tempString = System.getProperty("aspirinRetryInterval");
+			if( tempString != null )
+			{
+				retryInterval = Long.valueOf(tempString);
+			}else
+			{
+				tempString = System.getProperty(PARAM_DELIVERY_ATTEMPT_DELAY);
+				if( tempString != null )
+				{
+					retryInterval = Long.valueOf(tempString);
+				}
+			}
+		}
+		
+		tempString = props.getProperty(PARAM_DELIVERY_ATTEMPT_COUNT);
+		if( tempString != null )
+		{
+			maxAttempts = Integer.valueOf(tempString);
+		}else
+		{
+			// We need this to support backward compatibility
+			tempString = System.getProperty("aspirinMaxAttempts");
+			if( tempString != null )
+			{
+				maxAttempts = Integer.valueOf(tempString);
+			}else
+			{
+				tempString = System.getProperty(PARAM_DELIVERY_ATTEMPT_COUNT);
+				if( tempString != null )
+				{
+					maxAttempts = Integer.valueOf(tempString);
+				}
+			}
+		}
+		
+		tempString = props.getProperty(PARAM_DELIVERY_DEBUG);
+		if( tempString != null )
+			debugCommunication = ("true".equalsIgnoreCase(tempString) ) ? true : false;
+		
+		tempString = props.getProperty(PARAM_DELIVERY_THREADS_ACTIVE_MAX);
+		if( tempString != null )
+		{
+			deliveryThreads = Integer.valueOf(tempString);
+		}else
+		{
+			// We need this to support backward compatibility
+			tempString = System.getProperty("aspirinDeliverThreads");
+			if( tempString != null )
+			{
+				deliveryThreads = Integer.valueOf(tempString);
+			}else
+			{
+				tempString = System.getProperty(PARAM_DELIVERY_THREADS_ACTIVE_MAX);
+				if( tempString != null )
+				{
+					deliveryThreads = Integer.valueOf(tempString);
+				}
+			}
+		}
+		
+		tempString = props.getProperty(PARAM_DELIVERY_THREADS_IDLE_MAX);
+		if( tempString != null )
+		{
+			idleDeliveryThreads = Integer.valueOf(tempString);
+		}else
+		{
+			// We need this to support backward compatibility
+			tempString = System.getProperty("aspirinDeliverThreads");
+			if( tempString != null )
+			{
+				idleDeliveryThreads = Integer.valueOf(tempString);
+			}else
+			{
+				tempString = System.getProperty(PARAM_DELIVERY_THREADS_IDLE_MAX);
+				if( tempString != null )
+				{
+					idleDeliveryThreads = Integer.valueOf(tempString);
+				}
+			}
+		}
+		
+		tempString = props.getProperty(PARAM_DELIVERY_TIMEOUT);
+		if( tempString != null )
+			connectionTimeout = Integer.valueOf(tempString);
+		
+		tempString = props.getProperty(PARAM_POSTMASTER_EMAIL);
+		if( tempString != null )
+		{
+			setPostmasterEmail(tempString);
+		}else
+		{
+			tempString = System.getProperty("aspirinPostmaster");
+			if( tempString != null )
+			{
+				setPostmasterEmail(tempString);
+			}else
+			{
+				tempString = System.getProperty(PARAM_POSTMASTER_EMAIL);
+				if( tempString != null )
+				{
+					setPostmasterEmail(tempString);
+				}
+			}
+		}
+		
+		hostname = props.getProperty(
+				PARAM_HOSTNAME, 
+				System.getProperty("aspirinHostname", 
+						System.getProperty("mail.smtp.host",
+								System.getProperty(PARAM_HOSTNAME, hostname)
+						)
+				)
+		);
+		
+		encoding = props.getProperty(PARAM_ENCODING, encoding);
+		String loggerConfigName = props.getProperty(PARAM_LOGGER_NAME);
+		if( loggerConfigName != null && !loggerConfigName.equals(loggerName) )
+			log = LogFactory.getLog(loggerName);
+		loggerPrefix = props.getProperty(PARAM_LOGGER_PREFIX, loggerPrefix);
+	}
+	
 	/**
 	 *  
 	 */
 	private Configuration() {
-		if (System.getProperty("aspirinRetryInterval") != null) {
-			retryInterval = Long.parseLong(System
-					.getProperty("aspirinRetryInterval"));
-		}
-		if (System.getProperty("aspirinDeliverThreads") != null) {
-			deliveryThreads = Integer.parseInt(System
-					.getProperty("aspirinDeliverThreads"));
-		}
-		if (System.getProperty("aspirinPostmaster") != null) {
-			try {
-				postmaster = new MailAddress(System
-						.getProperty("aspirinPostmaster"));
-			} catch (ParseException e) {
-				log.error(e);
-				throw new RuntimeException(e);
-			} 
-		}
-		if (System.getProperty("aspirinMaxAttempts") != null) {
-			maxAttempts = Integer.parseInt(System
-					.getProperty("aspirinMaxAttempts"));
-		}
-		
-		if( System.getProperty("aspirinHostname") != null )
-		{
-			hostname = System.getProperty("aspirinHostname");
-		}else
-		if( System.getProperty("mail.smtp.host") != null )
-		{
-			hostname = System.getProperty("mail.smtp.host");
-		}
-		
+		init(new Properties());
 	}
 	/**
 	 * @return the number of milliseconds the system will wait before trying to
 	 *         resend an e-mail. This defaults to 5 minutes. A normail mail
 	 *         server would wait a few hours at least but Aspirin can't assume
 	 *         that an appilication will be open for a few hours.
+	 *         
+	 * @deprecated Use getDeliveryAttemptDelay() instead.
 	 */
 	public long getRetryInterval() {
 		return retryInterval;
@@ -109,6 +298,8 @@ public class Configuration {
 	/**
 	 * @param retryInterval
 	 *            The retryInterval to set.
+	 * 
+	 * @deprecated Use setDeliveryAttemptDelay() instead.
 	 */
 	public void setRetryInterval(long retryInterval) {
 		this.retryInterval = retryInterval;
@@ -116,6 +307,8 @@ public class Configuration {
 	/**
 	 * @return the number of threads in the thread pool available for mail
 	 *         delivery. This defaults to three.
+	 * 
+	 * @deprecated Use getDeliveryThreadsActiveMax() instead.
 	 */
 	public int getDeliveryThreads() {
 		return deliveryThreads;
@@ -127,17 +320,23 @@ public class Configuration {
 	 * 
 	 * @param threadCount
 	 *            the new number of threads to have available for mail delivery.
+	 * 
+	 * @deprecated Use setDeliveryThreadsActiveMax() instead.
+	 * 
 	 */
 	public void setDeliveryThreads(int threadCount) {
 		this.deliveryThreads = threadCount;
 	}
 	/**
-	 * @return the email address of the postmaster. This defaults to
-	 *         root@localhost.
+	 * @return The email address of the postmaster in a MailAddress object.
 	 */
-	public MailAddress getPostmaster() {
+	MailAddress getPostmaster() {
 		return postmaster;
 	}
+	/**
+	 * @deprecated Use setPostmasterEmail() instead.
+	 * @param postmasterAddress
+	 */
 	public void setPostmaster(String postmasterAddress) {
 		
 		try {
@@ -150,10 +349,15 @@ public class Configuration {
 	/**
 	 * @return int representing the number of times the system will attempt to
 	 *         send an e-mail before giving up.
+	 * @deprecated Use getDeliveryAttemptCount() instead.
 	 */
 	public int getMaxAttempts() {
 		return maxAttempts;
 	}
+	/**
+	 * @deprecated Use setDeliveryAttemptCount() instead.
+	 * @param maxAttempts
+	 */
 	public void setMaxAttempts(int maxAttempts) {
 		this.maxAttempts = maxAttempts;
 	}
@@ -161,15 +365,23 @@ public class Configuration {
 		return log;
 	}
 	
-		public String getHostname() {
+	public String getHostname() {
 		return hostname;
 	}
 	public void setHostname(String hostname) {
 		this.hostname = hostname;
 	}
+	/**
+	 * @deprecated Use isDeliveryDebug() instead.
+	 * @return
+	 */
 	public boolean isDebugCommunication() {
 		return debugCommunication;
 	}
+	/**
+	 * @deprecated Use setDeliveryDebug() instead.
+	 * @param debugCommunication
+	 */
 	public void setDebugCommunication(boolean debugCommunication) {
 		this.debugCommunication = debugCommunication;
 	}
@@ -179,17 +391,130 @@ public class Configuration {
 	public void setEncoding(String encoding) {
 		this.encoding = encoding;
 	}
+	/**
+	 * @deprecated Use getLoggerPrefix() instead.
+	 * @return
+	 */
 	public String getLogPrefix() {
-		return logPrefix;
+		return loggerPrefix;
 	}
+	/**
+	 * @deprecated Use setLoggerPrefix() instead.
+	 * @param logPrefix
+	 */
 	public void setLogPrefix(String logPrefix) {
-		this.logPrefix = logPrefix+" ";
+		this.loggerPrefix = logPrefix+" ";
 	}
+	/**
+	 * @deprecated Use getDeliveryTimeout() instead.
+	 * @return
+	 */
 	public int getConnectionTimeout() {
 		return connectionTimeout;
 	}
+	/**
+	 * @deprecated Use setDeliveryTimeout() instead.
+	 * @param connectionTimeout
+	 */
 	public void setConnectionTimeout(int connectionTimeout) {
 		this.connectionTimeout = connectionTimeout;
+	}
+
+	@Override
+	public int getDeliveryAttemptCount() {
+		return maxAttempts;
+	}
+
+	@Override
+	public int getDeliveryAttemptDelay() {
+		return (int)retryInterval;
+	}
+
+	@Override
+	public int getDeliveryThreadsActiveMax() {
+		return deliveryThreads;
+	}
+	
+	@Override
+	public int getDeliveryThreadsIdleMax() {
+		return idleDeliveryThreads;
+	}
+
+	@Override
+	public int getDeliveryTimeout() {
+		return connectionTimeout;
+	}
+
+	@Override
+	public String getLoggerName() {
+		return loggerName;
+	}
+
+	@Override
+	public String getLoggerPrefix() {
+		return loggerPrefix;
+	}
+	
+	@Override
+	public String getPostmasterEmail() {
+		return postmaster.toString();
+	}
+
+	@Override
+	public boolean isDeliveryDebug() {
+		return debugCommunication;
+	}
+
+	@Override
+	public void setDeliveryAttemptCount(int attemptCount) {
+		this.maxAttempts = attemptCount;
+	}
+
+	@Override
+	public void setDeliveryAttemptDelay(int delay) {
+		this.retryInterval = delay;
+	}
+
+	@Override
+	public void setDeliveryDebug(boolean debug) {
+		this.debugCommunication = debug;
+	}
+
+	@Override
+	public void setDeliveryThreadsActiveMax(int activeThreadsMax) {
+		this.deliveryThreads = activeThreadsMax;
+	}
+	
+	@Override
+	public void setDeliveryThreadsIdleMax(int idleThreadsMax) {
+		this.idleDeliveryThreads = idleThreadsMax;
+	}
+
+	@Override
+	public void setDeliveryTimeout(int timeout) {
+		this.connectionTimeout = timeout;
+	}
+
+//	@Override
+//	public void setLoggerName(String loggerName) {
+//		Configuration.loggerName = loggerName;
+//		log = LogFactory.getLog(loggerName); 
+//	}
+
+	@Override
+	public void setLoggerPrefix(String loggerPrefix) {
+		this.loggerPrefix = loggerPrefix;
+	}
+
+	@Override
+	public void setPostmasterEmail(String emailAddress) {
+		try
+		{
+			this.postmaster = new MailAddress(emailAddress);
+		}catch (ParseException e)
+		{
+			log.error(getClass().getSimpleName()+".setPostmasterEmail(): The email address is unparseable.", e);
+		}
 	}
 
 }
